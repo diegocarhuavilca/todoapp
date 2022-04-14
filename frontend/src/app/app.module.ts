a543ab46802c8c4d66c0f83a25b48579a49a365b
Diego.Carhuavilca <diego.carhuavilca@gmail.com>
2022-04-13 23:16:14 -0500

    [frontend] Add Notes template

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotesComponent } from './notes/notes.component';
+import { NoteComponent } from './notes/note/note.component';
+import { NoteListComponent } from './notes/note-list/note-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotesComponent,
+    NoteComponent,
+    NoteListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/notes/note-list/note-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-note-list',
+  templateUrl: './note-list.component.html',
+  styleUrls: ['./note-list.component.scss']
+})
+export class NoteListComponent implements OnInit {
+
+  constructor() { }
+
+  notes=['note1','note2','note3'];
+
+  ngOnInit(): void {
+  }
+
+}
